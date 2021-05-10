c3a4458f2e127a6e35d40e86d1cee8580f901620
amine-ship <83842904+amine-ship@users.noreply.github.com>
2021-05-10 20:31:33 +0300

    Update Black.java

--- othello/Black.java
+++ othello/Black.java
@@ -3,24 +3,24 @@
 public class Black {
 
  
-    public Game strategy(Game game, boolean done, int color) {
+    public Game strategie(Game game, boolean done, int color) {
         
-        return randStrategy(game,done,color);
+        return randStrategie(game,done,color);
     }
 
 
-    public Game randStrategy(Game game, boolean done, int color) {
+    public Game randStrategie(Game game, boolean done, int color) {
 
-        int row = (int)(Math.random()*(game.HEIGHT-2)) + 1;
-        int column = (int)(Math.random()*(game.WIDTH-2)) + 1;
+        int ligne = (int)(Math.random()*(game.HEIGHT-2)) + 1;
+        int colone = (int)(Math.random()*(game.WIDTH-2)) + 1;
         
-        while (!done && !game.legalMove(row,column,color,true)) {
-            row = (int)(Math.random()*(game.HEIGHT-2)) + 1;
-            column = (int)(Math.random()*(game.WIDTH-2)) + 1;
+        while (!done && !game.legalMove(ligne,colone,color,true)) {
+            ligne = (int)(Math.random()*(game.HEIGHT-2)) + 1;
+            colone = (int)(Math.random()*(game.WIDTH-2)) + 1;
         }
         
         if (!done)
-            game.board[row][column] = color;
+            game.bord[ligne][colone] = color;
 
         return game;
     }
